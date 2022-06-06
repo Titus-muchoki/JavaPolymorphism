36ebebb4fd49af87febf430dd7c9e2a9762b56cc
kajela <titoyut56@gmail.com>
2022-06-06 14:52:18 -0400

    add the initial file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,28 @@
-package org.example;public class Main {
+package org.example;
+
+ class Main {
+     public void myFurniture() {
+         System.out.println("i own a table");
+     }
+
+ }
+ class electronics extends Main {
+     public void myElectronics() {
+         System.out.println("i have a heavy machine wofer");
+     }
+
+ }
+ class place extends Main{
+     public void myArea() {
+         System.out.println("i live in mirema");
+     }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+         Main myObj = new Main();
+         electronics elec = new electronics();
+         place myPlace = new place();
+         myObj.myFurniture();
+         myPlace.myArea();
+         elec.myElectronics();
+       // System.out.println("Hello world!");
     }
 }
